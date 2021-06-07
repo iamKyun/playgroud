73e5b7dc5b460cd220fcda112ec83ce54798d6cd
Kyun <kyun@iamkyun.com>
2021-06-07 22:18:15 +0800

    init

---
+++ demo-service/src/main/java/com/iamkyun/playground/demo/user/User.java
@@ -0,0 +1,11 @@
+package com.iamkyun.playground.demo.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+    Integer id;
+    String name;
+}
