dbf3bbcd1a40f502c52496272bd32445665b1c3a
Kyun <kyun@iamkyun.com>
2021-06-08 23:27:13 +0800

    consul

--- services/course-service/src/main/java/com/iamkyun/playground/demo/CourseApplication.java
+++ services/course-service/src/main/java/com/iamkyun/playground/demo/CourseApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class DemoApplication {
+public class CourseApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(CourseApplication.class, args);
     }
 }
