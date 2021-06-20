db44b835f03c491d0c09476900d7d2fcbfcba01d
Kyun <kyun@iamkyun.com>
2021-06-21 00:09:24 +0800

    update

--- playground-starter/playground-database-starter/src/main/java/com/iamkyun/playground/core/database/DatabaseConfiguration.java
+++ playground-starter/playground-database-starter/src/main/java/com/iamkyun/playground/core/database/DatabaseConfiguration.java
@@ -1,13 +1,20 @@
-package com.iamkyun.playground.core.database.autoconfigure;
+package com.iamkyun.playground.core.database;
 
 import com.iamkyun.playground.core.database.mybatis.ReadWriteSplittingPlugin;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MyBatisConfiguration {
+public class DatabaseConfiguration {
+
+    /**
+     * 读写分离插件
+     */
+    @ConditionalOnProperty(prefix = "playground.database.read-write-splitting", name = "enabled", havingValue = "true")
     @Bean
     public ReadWriteSplittingPlugin readWriteSplittingPlugin() {
         return new ReadWriteSplittingPlugin();
     }
+
 }
