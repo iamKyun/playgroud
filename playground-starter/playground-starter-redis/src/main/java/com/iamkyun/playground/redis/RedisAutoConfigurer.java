75b606c965d598b3db9c06a4ae754cd043e691fb
Kyun <kyun@iamkyun.com>
2021-06-29 10:49:26 +0800

    update

---
+++ playground-starter/playground-starter-redis/src/main/java/com/iamkyun/playground/redis/RedisAutoConfigurer.java
@@ -0,0 +1,9 @@
+package com.iamkyun.playground.redis;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.iamkyun.playground.redis.api")
+public class RedisAutoConfigurer {
+
+}
--- playground-starter/playground-starter-redis/src/main/java/com/iamkyun/playground/redis/api/RedisController.java
+++ playground-starter/playground-starter-redis/src/main/java/com/iamkyun/playground/redis/api/RedisController.java
@@ -1,4 +1,4 @@
-package com.iamkyun.playground.redis;
+package com.iamkyun.playground.redis.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
